15cd580358dea2deb3e3f587c5c8e848b3507640
Jonas Eiselt <jonazeiselt@gmail.com>
2017-04-10 17:28:12 +0200

    testfall 4, 5 och 6 klara

--- HB25Tester/HB25Tester/src/main.c
+++ HB25Tester/HB25Tester/src/main.c
@@ -1,18 +1,23 @@
 /**
- * Author: Hadi Deknache and Jonas Eiselt
- * Created: 2017-04-10
- */
+* Author: Hadi Deknache and Jonas Eiselt
+* Created: 2017-04-10
+*/
 
 #include <asf.h>
 #include <inttypes.h>
 
 #define piob_baseAdress 0x400E1000U
 
-uint32_t *p_PIOB_PER = (uint32_t*) (piob_baseAdress + 0x0000U);
-uint32_t *p_PIOB_OER  = (uint32_t*) (piob_baseAdress + 0x0010U);
-uint32_t *p_PIOB_CODR = (uint32_t*) (piob_baseAdress + 0x0034U);
-uint32_t *p_PIOB_SODR = (uint32_t*) (piob_baseAdress + 0x0030U);
+uint32_t *const p_PIOB_PER = (uint32_t*) (piob_baseAdress + 0x0000U);
+uint32_t *const p_PIOB_OER  = (uint32_t*) (piob_baseAdress + 0x0010U);
+uint32_t *const p_PIOB_CODR = (uint32_t*) (piob_baseAdress + 0x0034U);
+uint32_t *const p_PIOB_SODR = (uint32_t*) (piob_baseAdress + 0x0030U);
 
+/*
+* Full reverse: 1.0 ms
+* Neutral (off): 1.5 ms
+* Full forward: 2.0 ms
+*/
 int main (void)
 {
 	/* Insert system clock initialization code here (sysclk_init()). */
@@ -25,18 +30,93 @@ int main (void)
 	*p_PIOB_PER |= (1<<26);
 	*p_PIOB_OER |= (1<<26);
 	
-	/* Send pulse to first motor controller */
-	*p_PIOB_SODR |= (1<<26);
-	delay_ms(1);
-	*p_PIOB_CODR |= (1<<26);
+	while (1)
+	{
+		for (uint32_t i = 1000; i<=1500;i++)
+		{
+			/* Send pulse to first motor controller */
+			*p_PIOB_SODR |= (1<<26);
+			delay_us(i);
+			*p_PIOB_CODR |= (1<<26);
+			
+			/* Mode 2 */
+			delay_us(1100);
+			
+			/* Send pulse to second motor controller */
+			*p_PIOB_SODR |= (1<<26);
+			delay_us(i);
+			*p_PIOB_CODR |= (1<<26);
+			
+			delay_us(5250);
+		}
+		
+		
+		delay_s(3);
+		
+		for (uint32_t i = 1500; i<=2000;i++)
+		{
+			/* Send pulse to first motor controller */
+			*p_PIOB_SODR |= (1<<26);
+			delay_us(i);
+			*p_PIOB_CODR |= (1<<26);
+			
+			/* Mode 2 */
+			delay_us(1100);
+			
+			/* Send pulse to second motor controller */
+			*p_PIOB_SODR |= (1<<26);
+			delay_us(i);
+			*p_PIOB_CODR |= (1<<26);
+			
+			delay_us(5250);
+		}
+		
+		/* Send pulse to first motor controller */
+		*p_PIOB_SODR |= (1<<26);
+		delay_us(1500);
+		*p_PIOB_CODR |= (1<<26);
+		
+		/* Mode 2 */
+		delay_us(1100);
+		
+		/* Send pulse to second motor controller */
+		*p_PIOB_SODR |= (1<<26);
+		delay_us(1500);
+		*p_PIOB_CODR |= (1<<26);
+		
+		delay_s(3);
+	}
 	
-	/* Mode 2 */
-	delay_ms(1.10);
 	
-	/* Send pulse to second motor controller */
-	*p_PIOB_SODR |= (1<<26);
-	delay_ms(1);
-	*p_PIOB_CODR |= (1<<26);
+	while (1)
+	{
+		/* Send pulse to first motor controller */
+		*p_PIOB_SODR |= (1<<26);
+		delay_ms(1);
+		*p_PIOB_CODR |= (1<<26);
+		
+		delay_s(2);
+		
+		/* Send pulse to first motor controller */
+		*p_PIOB_SODR |= (1<<26);
+		delay_us(1500);
+		*p_PIOB_CODR |= (1<<26);
+		
+		delay_s(2);
+		
+		/* Send pulse to first motor controller */
+		*p_PIOB_SODR |= (1<<26);
+		delay_ms(2);
+		*p_PIOB_CODR |= (1<<26);
+		
+		delay_s(2);
+		
+		/* Send pulse to first motor controller */
+		*p_PIOB_SODR |= (1<<26);
+		delay_us(1500);
+		*p_PIOB_CODR |= (1<<26);
+		
+		delay_s(2);
+	}
 	
-	while (1);
 }
