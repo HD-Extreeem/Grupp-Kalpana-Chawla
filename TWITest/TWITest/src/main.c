07637ff269a7a453aef27ea06736c84bd876b7de
Jonas Eiselt <jonazeiselt@gmail.com>
2017-04-29 22:05:48 +0200

    send byte doesn't work yet..

--- TWITest/TWITest/src/main.c
+++ TWITest/TWITest/src/main.c
@@ -8,7 +8,9 @@
 #include <twi.h>
 #include "consoleFunctions.h"
 
-#define TWI_CLK 40000
+#define TWI_CLK 400000
+#define UNO_ADDRESS 0x50
+#define DATA_LENGTH 1
 
 int main (void)
 {
@@ -19,9 +21,7 @@ int main (void)
 	configure_console();
 	
 	/* Enable TWI peripheral clock */
-	pmc_enable_periph_clk(TWI0);
-	
-	/* Enable required TWI PIOs */
+	pmc_enable_periph_clk(ID_TWI0);
 	
 	/* Enable TWI master mode */
 	twi_enable_master_mode(TWI0);
@@ -33,26 +33,57 @@ int main (void)
 	
 	/* Configure TWI as master*/
 	uint32_t status = twi_master_init(TWI0, &p_opt);
+	if (status != TWI_SUCCESS) 
+	{
+		printf("Failed to initialize master\r\n");
+	}
+	else 
+	{
+		printf("Initialized master\r\n");
+	}
+
+	static uint8_t tx_buffer[] = "Hello";
+	static uint8_t rx_buffer[16] = {0};
+	printf("Here : 1\r\n");
 	
 	/* Configure packet to be sent */
-	twi_packet_t p_packet;
-	p_packet.addr[0] = 0;
-	p_packet.addr[1] = 0;
-	p_packet.addr[2] = 0;
-	p_packet.addr_length = 0;
-	p_packet.buffer = tx_data;
-	p_packet.chip = UNO_ADDRESS;
-	p_packet.length = DATA_LENGTH;
+	twi_packet_t tx_packet;
+	tx_packet.chip = 0x50;
+	tx_packet.addr[0] = 0>>8;
+	tx_packet.addr[1] = 0;
+	tx_packet.addr_length = 1;
+	tx_packet.buffer = (uint8_t*) tx_buffer;
+	tx_packet.length = (sizeof(tx_buffer)/sizeof(uint8_t));
+	
+	/* Configure packet to be received */
+	twi_packet_t rx_packet;
+	rx_packet.chip = 0x50;
+	rx_packet.addr[0] = 22;
+	rx_packet.addr[1] = 0;
+	rx_packet.addr[2] = 0;
+	rx_packet.addr_length = 1;
+	rx_packet.buffer = (uint8_t*) rx_buffer;
+	rx_packet.length = (30/sizeof(uint8_t));
+	
+	printf("Here : 2\r\n");
 	
 	/* Send data to slave device */
-	/*status = twi_master_write(TWI0, &p_packet);
-
-	/* Receive data from slave device */
-	twi_master_read(TWI0, &p_packet);
-	for (uint32_t i = 0; i < DATA_LENGTH; i++) 
+	status = twi_master_write(TWI0, &tx_packet);
+	printf("Here : 3\r\n");
+	if (status != TWI_SUCCESS)
 	{
-		printf("%d", tx_buffer[i]);
+		printf("Failed to send data\r\n");
 	}
+	
+	/* Receive data from slave device */
+	status = twi_master_read(TWI0, &rx_packet);
+	if (status == TWI_SUCCESS)
+		printf("Received something!");
+		
+	/*for (uint32_t i = 0; i < (sizeof(tx_data)/sizeof(uint8_t)); i++) 
+	{
+		// printf("Received: %d\n\r", (uint8_t*) tx_data);
+	}*/
 	
 	while (1);
 }
