77a290ef8d3553b545e954620a715769e9837751
Hadi Deknache <hadi.deknache@gmail.com>
2017-05-03 10:39:15 +0200

    Kommenterade kod för moveto samt sourcefilen

--- reglering-v1/PwmTest/PwmTest/src/PID_move.c
+++ reglering-v1/PwmTest/PwmTest/src/PID_move.c
@@ -1,7 +1,8 @@
 /*
  * PID_move.c
- *
  * Created: 2017-04-26 14:38:37
+ * This source file handle the main regulation for wheels and rotation
+ * for a certain degree.
  *  Author: Yurdaer Dalkic && Hadi Deknache
  */
 #include <asf.h>
@@ -9,80 +10,96 @@
 #include "InterruptStepCounter.h"
 #include "PID_move.h"
 
-extern uint16_t counter_1;
-extern uint16_t counter_2;
-extern bool c1Loop;
-extern bool c2Loop;
 
+extern uint16_t counter_1; //Encoder counter for rightWheel
+extern uint16_t counter_2; //Encoder counter for leftWheel
+extern bool c1Loop; //Boolean for checking if encoder counter 1 triggered interrupt
+extern bool c2Loop; //Boolean for checking if encoder counter 2 triggered interrupt
 
+/**
+This method moveTo handle position moving for a certain distance and direction
+Method uses a PID controller for smoother movment of the robot
+	@param distance the distance which the robot need to move in centimeters
+	@param direction the direction robot need to move forward or backwards
+**/
 void moveTo (int distance, int direction){
-	float totalPulses = (distance/1.38);
-	uint16_t speed = 1700;
-	int proportionalError = 0;
-    int derivativeError=0;
-    int integralError=0;
-	int referenceValue = 0;
+	float totalPulses = (distance/1.38); //Calculate the total pulses needed to move to destination
+	uint16_t speed = 1700; // Set speed for moving the robot
+	int proportionalError = 0; //P-controller error variable
+  int derivativeError=0; //D-controller error variable
+  int integralError=0; //I-controller error variable
+	int referenceValue = 0; //
 	int measurementValue=0;
-	int controlValue=0;
-    int iPart=0;
-    int dPart=0;
-	int kp=10;
-    int kd=2;
-    int ki=2;
-	float totMovement = 0;
+	int controlValue=0; //Variable to store total value for PID-controller
+  int iPart=0; //Variable for the I-controller to keep calculated error*gain
+  int dPart=0; //Variable for the D-controller to keep calculated error*gain
+	int kp=10; //Gain for the P-controller
+  int kd=2; //Gain for the D-controller
+  int ki=2; //Gain for the I-controller
+	float totMovement = 0; //Variable to store totalmovement during the transportation
 	delay_us(300);
-	reset_Counter();
+	reset_Counter(); //Reset counter for encoders for wheel to ensure counter reseted for next movement
 	delay_us(300);
+
+	// Controlls if moving forward or backwards
 	if ( direction!=1 || direction!=-1 )
 	{
 		direction=1;
 	}
 
-	
+		/**
+		This while loop runs until robot reaches its destination
+		checks if totalmovemnt pulses is the same or more than the total pulses needed
+		**/
     while (totMovement <= totalPulses) {
 		delay_ms(1);
+			/**
+			Checks if both encoder have counted up since last time
+			if counted, then regulates the need values from encoder
+			**/
       if (c1Loop == true && c2Loop == true) {
 		totMovement = totMovement + ((counter_1+counter_2)/2);
 		delay_ms(1);
-        measurementValue = (counter_2-counter_1);
+        measurementValue = (counter_2-counter_1);// Calculates the error diffferce
 		delay_us(500);
-        reset_Counter();
+        reset_Counter();//Reset counter for next regulation later
 		delay_us(500);
-        dPart = (kd*(proportionalError-derivativeError));
+        dPart = (kd*(proportionalError-derivativeError)); //Calculates d-controller gain
 		delay_us(500);
-        iPart = (ki*integralError);
+        iPart = (ki*integralError); //Calculates i-controller gain
 		delay_us(500);
-		proportionalError = (referenceValue - measurementValue);
+		proportionalError = (referenceValue - measurementValue); // Calculates p-controller gain
 		delay_us(500);
-		controlValue = ((kp*proportionalError)+iPart+dPart);
+		controlValue = ((kp*proportionalError)+iPart+dPart); //Total regulation for PID calculate new value for correcting the error
 		delay_us(500);
-// 		if ((totMovement/totalPulses)>= 0.8)
-// 		{
-// 			speed = 1650;
-// 		}
-// 		else if ((totMovement/totalPulses)>= 0.9)
-// 		{
-// 			speed = 1580;
-// 		}
-		
-		rightWheel((speed+controlValue));
-		leftWheel((speed-controlValue));
+		 /**
+		 	Check if almost reached the destination to slow down and make a smoother brake
+		 **/
+ 		if ((totMovement/totalPulses)>= 0.7)
+ 		{
+ 			speed = speed - ((totMovement/totalPulses)*150) ;
+ 		}
+		//Else same speed set
+ 		else
+ 		{
+ 			speed = speed;
+ 		}
+
+		rightWheel((speed+controlValue));//New speed for rightWheel
+		leftWheel((speed-controlValue));//New speed for leftWheel
 		delay_us(500);
 	    //error=error*-1;
         derivativeError = proportionalError;
 		delay_us(500);
         integralError = (integralError+proportionalError);
 		delay_us(500);
-        c1Loop = false;
-        c2Loop = false;
+        c1Loop = false; //Loop finished to prevent from running loop again
+        c2Loop = false;//Loop finished to prevent from running loop again
 		delay_us(500);
 		//printf("%d\n",totMovement);
       }
 
 	}
-	rightWheel(1500);
-	leftWheel(1500);
-	delay_s(2);
 }
 
 
