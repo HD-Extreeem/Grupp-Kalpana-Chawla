374f7cd5779bcb0ea747aae825385543a3770bcc
Jonas Eiselt <jonazeiselt@gmail.com>
2017-05-08 12:30:57 +0200

    switch/case-sats på Uno:t och Due:n

--- DueUnoTest/UnoReceiver/UnoReceiver.ino
+++ DueUnoTest/UnoReceiver/UnoReceiver.ino
@@ -10,12 +10,16 @@
 
 #define DEVICE_ADDRESS 0x50
 #define BAUD_RATE 115200
-#define DATA_LENGTH 2
+
+#define RX_DATA_LENGTH 1
+#define TX_DATA_LENGTH 3
+
+#define CRANE 0x33
 
 boolean received = false;
 
-uint8_t rx_buf[2];
-uint8_t tx_buf[2];
+uint8_t rx_buf[RX_DATA_LENGTH];
+uint8_t tx_buf[TX_DATA_LENGTH];
 
 void setup()
 {
@@ -28,9 +32,9 @@ void setup()
 
 void loop()
 {
-  tx_buf[DATA_LENGTH];
-  if (received)
-  {
+  /*tx_buf[DATA_LENGTH];
+    if (received)
+    {
     for (int i = 0; i < DATA_LENGTH; i++)
     {
       tx_buf[i] = rx_buf[i];
@@ -42,28 +46,15 @@ void loop()
       Serial.print(rx_buf[i], HEX);
       Serial.print(' ');
     }
-    Serial.println();
+    Serial.println();*/
 
-    /* Doesn't make any difference with beginTransmission */
-    /*Wire.beginTransmission(DEVICE_ADDRESS);
+  /* Doesn't make any difference with beginTransmission */
+  /*Wire.beginTransmission(DEVICE_ADDRESS);
     Wire.write(tx_buf, sizeof(tx_buf));
     Wire.endTransmission();*/
 
-    /*
-       Wire.write((tx_buf[1] >> 8) & 0xff);
-       Wire.write(tx_buf[0] & 0xff);
-    */
-
-    /* MSB byte */
-    // Wire.write((buf[1] >> 24) & 0xff);
-    // Wire.write((buf[1] >> 16) & 0xff);
-
-    /* LSB byte */
-    // Wire.write((buf[0] >> 8) & 0xff);
-    // Wire.write(buf[0] & 0xff);
-
-    received = false;
-  }
+  // received = false;
+  // }
 }
 
 void receiveEvent(int howMany)
@@ -77,10 +68,128 @@ void receiveEvent(int howMany)
   received = true;
 }
 
-/* Use requestEvent to send.. sending from loop doesn't seem to work.. */
+/*
+   Use requestEvent to send.. sending from loop doesn't seem to work..
+*/
 void requestEvent()
 {
-  Serial.println("Hello?");
-  Wire.write(0x55);
-  Wire.write(0x45);
+  tx_buf[TX_DATA_LENGTH] = {0};
+  
+  Serial.print("Got 0x");
+  Serial.print(rx_buf[0], HEX);
+  Serial.println();
+
+  switch (rx_buf[0])
+  {
+    case 0x20:
+      // Identification
+      tx_buf[0] = 0x10;
+      tx_buf[1] = 0x10;
+      tx_buf[2] = 0x10;
+      break;
+    case 0x21:
+      // Lift cube
+      if (!liftCube())
+      {
+        // Failed to lift cube
+        tx_buf[0] = 0x15;
+      }
+      else
+      {
+        tx_buf[0] = 0x14;
+      }
+      tx_buf[1] = 0x10;
+      tx_buf[2] = 0x10;
+      break;
+    case 0x22:
+      // Lift glass
+      if (!liftGlass())
+      {
+        tx_buf[0] = 0x15;
+      }
+      else
+      {
+        tx_buf[0] = 0x14;
+      }
+      tx_buf[1] = 0x10;
+      tx_buf[2] = 0x10;
+      break;
+    case 0x23:
+      // Lift sock
+      if (!liftSock())
+      {
+        tx_buf[0] = 0x15;
+      }
+      else
+      {
+        tx_buf[0] = 0x14;
+      }
+      tx_buf[1] = 0x10;
+      tx_buf[2] = 0x10;
+      break;
+    case 0x24:
+      // Cancel lift
+      cancelLift();
+      break;
+    case 0x25:
+      // Return object
+      if (!returnObject())
+      {
+        // Failed to return object
+        tx_buf[0] = 0x17;
+      }
+      else
+      {
+        tx_buf[0] = 0x16;
+      }
+      tx_buf[1] = 0x10;
+      tx_buf[2] = 0x10;
+      break;
+    default:
+      // Do nothing...
+      break;
+  }
+  Wire.write(tx_buf, sizeof(tx_buf));
+  Serial.print("Sent ");
+  for (int i = 0; i < sizeof(tx_buf); i++)
+  {
+    Serial.print(tx_buf[i], HEX);
+    Serial.print(' ');
+  }
+  Serial.println("\n");
+}
+
+boolean liftCube()
+{
+  /* Program code goes here... */
+  Serial.println("Lifting cube...");
+  return true;
+}
+
+boolean liftGlass()
+{
+  /* Program code goes here... */
+  Serial.println("Lifting glass...");
+  return true;
+}
+
+boolean liftSock()
+{
+  /* Program code goes here... */
+  Serial.println("Lifting sock...");
+  return true;
+}
+
+void cancelLift()
+{
+  /* Program code goes here... */
+  Serial.println("Canceling lift...");
+  return true;
+}
+
+boolean returnObject()
+{
+  /* Program code goes here... */
+  Serial.println("Returning object...");
+  return true;
 }
