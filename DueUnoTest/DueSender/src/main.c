a76b34a505d71847d9dddf4f2851a50191933890
Jonas Eiselt <jonazeiselt@gmail.com>
2017-05-08 15:38:01 +0200

    merge fix

--- DueUnoTest/DueSender/src/main.c
+++ DueUnoTest/DueSender/src/main.c
@@ -13,15 +13,23 @@
 #include "twi.h"
 #include "led.h"
 
-#define UNO_BUS_ADDR 0x50        // TWI slave bus address
-#define TWI_SPEED 50000			 // TWI data transfer rate
+#define UNO_BUS_ADDR 0x50       // TWI slave bus address
+#define TWI_SPEED 100000		// TWI data transfer rate
 
-#define DATA_LENGTH sizeof(tx_buffer)
-static const uint8_t tx_buffer[] = {0x55, 0x55};
-static uint8_t rx_buffer[DATA_LENGTH] = {0};
+#define TX_DATA_LENGTH sizeof(tx_buffer)
+#define RX_DATA_LENGTH 3
+
+static uint8_t tx_buffer[] = {0x20};
+static uint8_t rx_buffer[RX_DATA_LENGTH] = {0};
 
 void configure_twi(void);
 void indicate(void);
+
+Bool crane_init(void);
+
+void send_data(uint8_t request_byte);
+void request_data(void);
+void check_data(void);
 
 static twi_package_t tx_packet;
 static twi_package_t rx_packet;
@@ -34,21 +42,15 @@ int main(void)
 	configure_twi();
 	// indicate();
 	
-	// Performs a multi-byte write access then check the result.
-	while (twi_master_write(TWI0, &tx_packet) != TWI_SUCCESS);
-	// indicate();
-	
-	// Performs a multi-byte read access then check the result.
-	while (twi_master_read(TWI0, &rx_packet) != TWI_SUCCESS);
-	// indicate();
-	
-	/*if (rx_buffer[0] == 0)
-	{
+	/* Crane init */
+	while (!crane_init());
 	indicate();
-	}*/
 	
-	while (twi_master_write(TWI0, &rx_packet) != TWI_SUCCESS);
-	indicate();
+	send_data(0x10);
+	request_data();
+	
+	// request_data();
+	// check_data();
 	
 	while(1);
 }
@@ -68,7 +70,7 @@ void configure_twi()
 	tx_packet.addr_length = 0;						// TWI slave memory address data size
 	tx_packet.chip = UNO_BUS_ADDR;					// TWI slave bus address
 	tx_packet.buffer = (void*) tx_buffer;			// transfer data source buffer
-	tx_packet.length = DATA_LENGTH;					// transfer data size (bytes)
+	tx_packet.length = TX_DATA_LENGTH;					// transfer data size (bytes)
 
 	/* Configures packet to be received */
 	rx_packet.addr[0] = 0;							// TWI slave memory address data MSB
@@ -76,7 +78,21 @@ void configure_twi()
 	rx_packet.addr_length = 0;						// TWI slave memory address data size
 	rx_packet.chip = UNO_BUS_ADDR;					// TWI slave bus address
 	rx_packet.buffer = (void*) rx_buffer;		// transfer data destination buffer
-	rx_packet.length = DATA_LENGTH;					// transfer data size (bytes)
+	rx_packet.length = RX_DATA_LENGTH;					// transfer data size (bytes)
+}
+
+Bool crane_init()
+{
+	send_data(0x20);
+	request_data();
+
+	// indicate();
+	
+	/* Check received data */
+	check_data();
+	request_data();
+	
+	return true;
 }
 
 /* Signals the user via the on-board LED (marked L) */
@@ -88,5 +104,71 @@ void indicate()
 		delay_ms(100);
 		LED_Off(LED0_GPIO);
 		delay_ms(100);
+	}
+}
+
+void send_data(uint8_t request_byte)
+{
+	tx_buffer[0] = request_byte;
+	
+	// Performs a multi-byte write access then check the result.
+	while (twi_master_write(TWI0, &tx_packet) != TWI_SUCCESS);
+	// indicate();
+}
+
+void request_data()
+{
+	// Performs a multi-byte read access then check the result.
+	while (twi_master_read(TWI0, &rx_packet) != TWI_SUCCESS);
+	// indicate();
+}
+
+void check_data()
+{
+	switch (rx_buffer[0])
+	{
+		case 0x10:
+		/* Identification */
+		send_data(0x21);
+		break;
+		case 0x11:
+		/* Postion relative to object */
+		send_data(0x11);
+		break;
+		case 0x12:
+		/* Position relative to box-edge */
+		send_data(0x12);
+		break;
+		case 0x13:
+		/* Max velocity (mm/s) */
+		send_data(0x13);
+		break;
+		case 0x14:
+		/* Lift success */
+		send_data(0x14);
+		break;
+		case 0x15:
+		/* Lift failed */
+		send_data(0x15);
+		break;
+		case 0x16:
+		/* Return success */
+		send_data(0x16);
+		break;
+		case 0x17:
+		/* Return failed */
+		send_data(0x17);
+		break;
+		case 0x18:
+		/* Adjust position */
+		send_data(0x18);
+		break;
+		case 0x19:
+		/* Cancel adjustment */
+		send_data(0x19);
+		break;
+		default:
+		// Do nothing...
+		break;
 	}
 }
