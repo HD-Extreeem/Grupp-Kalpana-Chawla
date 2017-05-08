c7cf6c30fb2e104155d140cb9fdbdc06a97a4bee
Unknown <yurdaer.dalkic@hotmail.com>
2017-05-08 13:44:22 +0200

    mergeeee

---
+++ RTOS Komplett/TestSchema1/src/PID_move.h
@@ -0,0 +1,21 @@
+/*
+ * PID_move.h
+ *
+ * Created: 2017-04-26 14:38:22
+ *  Author: Yurdaer Dalkic && Hadi Deknache
+ */
+
+
+#ifndef PID_MOVE_H_
+#define PID_MOVE_H_
+
+uint16_t counter_1; //Encoder counter for rightWheel
+uint16_t counter_2; //Encoder counter for leftWheel
+bool c1Loop; //Boolean for checking if encoder counter 1 triggered interrupt
+bool c2Loop; //Boolean for checking if encoder counter 2 triggered interrupt
+
+void moveTo (int distance, int direction);
+void rotation (int degree, int rotationSpeed);
+
+
+#endif /* PID_MOVE_H_ */
---
+++ RTOS Komplett/TestSchema1/src/Task_Move.h
@@ -0,0 +1,20 @@
+/*
+ * Task_Move.h
+ *
+ * Created: 2017-04-20 14:11:43
+ *  Author: Désirée Jönsson och Jonas Eiselt
+ */ 
+
+
+#ifndef TASK_MOVE_H_
+#define TASK_MOVE_H_
+
+
+#define TASK_MOVE_STACK_SIZE     (1024/sizeof(portSTACK_TYPE))
+#define TASK_MOVE_STACK_PRIORITY     (2)//tskIDLE_PRIORITY
+int objects[3][2];
+int lastPosition [1][1];
+int presentPosition [1][1];
+void task_move(void *pvParameters);
+
+#endif /* TASK_MOVE_H_ */
