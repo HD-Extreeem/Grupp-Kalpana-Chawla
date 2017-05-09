5d1b25e0361241e816e0b818ffe087bb43b8e85b
DesireeJ <desiiree__@hotmail.com>
2017-05-09 11:57:41 +0200

    In progress FreeRTOS TWI istället för "bara" TWI

--- SklettKlart/TestSchema1/src/Task_UnoComm.c
+++ SklettKlart/TestSchema1/src/Task_UnoComm.c
@@ -1,14 +1,13 @@
 /**
- * Task_UnoComm.c
- * 
- * Created: 2017-04-20 16:25:09
- *  Author: Desiree
- */
+* Task_UnoComm.c
+*
+* Created: 2017-04-20 16:25:09
+*  Author: Desiree
+*/
 
 #include <asf.h>
 #include "Task_UnoComm.h"
 #include <FreeRTOS.h>
-#include "twi.h"
 
 
 
@@ -21,68 +20,127 @@
 static uint8_t tx_buffer[] = {0x20};
 static uint8_t rx_buffer[RX_DATA_LENGTH] = {0};
 
-void configure_twi(void);
+//void configure_twi(void);
 void indicate(void);
 
 Bool crane_init(void);
 
-void send_data(uint8_t request_byte);
-void request_data(void);
-void check_data(void);
+//void send_data(uint8_t request_byte);
+//void request_data(void);
+//void check_data(void);
 
-static twi_package_t tx_packet;
-static twi_package_t rx_packet;
+//static twi_package_t tx_packet;
+//static twi_package_t rx_packet;
+
+freertos_twi_if freertos_twi;
+freertos_peripheral_options_t driver_options;
+
+twi_packet_t tx_packet;
+twi_packet_t rx_packet;
 
 
 
 void task_unoComm(void *pvParameters)
 {
 
+
+
+	configure_twi();
 	portTickType xLastWakeTime;
 	const portTickType xTimeIncrement = 500;
+	//const char req[] = "00000000";
 	
 	xLastWakeTime = xTaskGetTickCount(); // Initialize the xLastWakeTime variable with the current time.
 	
 	while (1)
 	{
-		vTaskDelayUntil(&xLastWakeTime, xTimeIncrement);	// Wait for the next cycle.	
-		
-		while (!crane_init());
+		vTaskDelayUntil(&xLastWakeTime, xTimeIncrement);	// Wait for the next cycle.
+		printf("UnoComm task\r\n");
+		//while (!crane_init());
 		//indicate();
-
-		send_data(0x10);
-		request_data();
-		}	
+		//crane_init();
+	
+		
+		freertos_twi_write_packet(freertos_twi,tx_packet,10);
+		//send_data(0x10);
+		//request_data();
+			printf("halvvägs\n");
+		printf("UnoComm tallllllsk\r\n");
+	}
 }
 
 void configure_twi()
 {
-	/* Initializes TWI master options */
-	twi_master_options_t opt;
-	opt.speed = TWI_SPEED;
+	driver_options = {NULL,0,0x0e,TWI_I2C_MASTER,(USE_TX_ACCESS_MUTEX | USE_RX_ACCESS_MUTEX)};
+	freertos_twi = freertos_twi_master_init(TWI0, &driver_options);
+	if (freertos_twi != NULL)
+	{
+		twi_set_speed(TWI0,TWI_SPEED,84000000);
+	}
 
-	/* Initializes the TWI master driver */
-	twi_master_setup(TWI0, &opt);
 
-	/* Configures packet to be transmitted */
-	tx_packet.addr[0] = 0;							// TWI slave memory address data MSB
-	tx_packet.addr[1] = 0;							// TWI slave memory address data LSB
-	tx_packet.addr_length = 0;						// TWI slave memory address data size
-	tx_packet.chip = UNO_BUS_ADDR;					// TWI slave bus address
-	tx_packet.buffer = (void*) tx_buffer;			// transfer data source buffer
-	tx_packet.length = TX_DATA_LENGTH;					// transfer data size (bytes)
+	twi_packet_t tx_packet={
 
-	/* Configures packet to be received */
-	rx_packet.addr[0] = 0;							// TWI slave memory address data MSB
-	rx_packet.addr[1] = 0;							// TWI slave memory address data LSB
-	rx_packet.addr_length = 0;						// TWI slave memory address data size
-	rx_packet.chip = UNO_BUS_ADDR;					// TWI slave bus address
-	rx_packet.buffer = (void*) rx_buffer;		// transfer data destination buffer
-	rx_packet.length = RX_DATA_LENGTH;					// transfer data size (bytes)
+		/* Configures packet to be transmitted */
+		.addr[0] = 0,							// TWI slave memory address data MSB
+		.addr[1] = 0,							// TWI slave memory address data LSB
+		.addr_length = 0,						// TWI slave memory address data size
+		.chip = UNO_BUS_ADDR,					// TWI slave bus address
+		.buffer = (void*) tx_buffer,			// transfer data source buffer
+		.length = TX_DATA_LENGTH					// transfer data size (bytes)
+	};
+
 }
+
+//-------
+
+
+
+/* Initializes TWI master options */
+//	twi_master_options_t opt;
+//	opt.speed = TWI_SPEED;
+
+/* Initializes the TWI master driver */
+//	twi_master_setup(TWI0, &opt);
+
+/* Configures packet to be transmitted */
+//	tx_packet.addr[0] = 0;							// TWI slave memory address data MSB
+//	tx_packet.addr[1] = 0;							// TWI slave memory address data LSB
+//	tx_packet.addr_length = 0;						// TWI slave memory address data size
+//	tx_packet.chip = UNO_BUS_ADDR;					// TWI slave bus address
+//	tx_packet.buffer = (void*) tx_buffer;			// transfer data source buffer
+//	tx_packet.length = TX_DATA_LENGTH;					// transfer data size (bytes)
+
+/* Configures packet to be received */
+//	rx_packet.addr[0] = 0;							// TWI slave memory address data MSB
+//	rx_packet.addr[1] = 0;							// TWI slave memory address data LSB
+//	rx_packet.addr_length = 0;						// TWI slave memory address data size
+//	rx_packet.chip = UNO_BUS_ADDR;					// TWI slave bus address
+//	rx_packet.buffer = (void*) rx_buffer;		// transfer data destination buffer
+//	rx_packet.length = RX_DATA_LENGTH;					// transfer data size (bytes)
+
+///////////////////////----------------
+
+
+// Declare the variables used as parameters to the
+// freertos_twi_master_init() function.
+// Handle used to access the initialized port by other FreeRTOS ASF functions.
+
+
+// Configuration structure.
+
+
+
+//freertos_peripheral_options_t twiOptions;
+//twiOptions.receive_buffer=(void*) rx_buffer;		// transfer data destination buffer
+//twiOptions.receive_buffer_size=RX_DATA_LENGTH;
+//twiOptions.operation_mode=TWI_I2C_MASTER;
+
+
 
 Bool crane_init()
 {
+	printf("crane_init\r\n");
 	send_data(0x20);
 	request_data();
 
@@ -98,11 +156,18 @@ Bool crane_init()
 
 void send_data(uint8_t request_byte)
 {
+	
+	printf("crankkkke_init\r\n");
 	tx_buffer[0] = request_byte;
 
 	// Performs a multi-byte write access then check the result.
-	while (twi_master_write(TWI0, &tx_packet) != TWI_SUCCESS);
+	//while (twi_master_write(TWI0, &tx_packet) != TWI_SUCCESS);
+	
+	//	while (freertos_twi_master_init(TWI0,&tx_packet)!= TWI_SUCCESS);
 	// indicate();
+	
+	
+	
 }
 
 void request_data()
