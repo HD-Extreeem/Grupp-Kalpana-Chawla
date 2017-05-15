2f15db62029faa5e1fa086a96301ea9030226259
Hadi Deknache <hc_boy_land@yahoo.com>
2017-05-15 15:18:56 +0200

    Skapade en ny mapp för RTOS med logik i statemachine för att köra alla armar

---
+++ RTOS v.3/TestSchema1/src/Task_UnoComm.c
@@ -0,0 +1,27 @@
+/**
+* Task_UnoComm.c
+*
+* Created: 2017-04-20 16:25:09
+*  Author: Desiree
+*/
+
+#include <asf.h>
+#include "Task_UnoComm.h"
+#include <FreeRTOS.h>
+#include "task.h"
+Bool liftProcessFinished;
+
+void task_unoComm(void *pvParameters)
+{
+	
+	portTickType xLastWakeTime;
+	const portTickType xTimeIncrement = 50;
+
+	xLastWakeTime = xTaskGetTickCount(); // Initialize the xLastWakeTime variable with the current time.
+
+	while (1)
+	{
+		vTaskDelayUntil(&xLastWakeTime, xTimeIncrement);	// Wait for the next cycle after have finished everything
+		liftProcessFinished = true;
+	}
+}
---
+++ RTOS v.3/TestSchema1/src/consoleFunctions.h
@@ -0,0 +1,13 @@
+/*
+ * consoleFunctions.h
+ *
+ * Created: 2017-04
+ * Author: Kalapan
+ */ 
+
+#ifndef CONSOLEFUNCTIONS_H_
+#define CONSOLEFUNCTIONS_H_
+
+void configureConsole(void);
+
+#endif /* CONSOLEFUNCTIONS_H_ */
