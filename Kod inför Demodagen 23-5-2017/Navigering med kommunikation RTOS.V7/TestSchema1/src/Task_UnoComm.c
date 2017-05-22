f9dc1eee05aa787ad732a5e8625f11f9ae9e2ed6
Hadi Deknache <hc_boy_land@yahoo.com>
2017-05-23 00:01:19 +0200

    FREERTOS för navigeringen för demodagen imorgon, kod justerade för kommunikation, redigerade efter fungerande positioneringsdata

---
+++ Kod inför Demodagen 23-5-2017/Navigering med kommunikation RTOS.V7/TestSchema1/src/Task_UnoComm.c
@@ -0,0 +1,205 @@
+/**
+* Task_UnoComm.c
+*
+* Created: 2017-04-20 16:25:09
+* Author: Desiree
+*
+* Modified by Hadi Deknache and Jonas Eiselt
+*/
+
+#include <asf.h>
+#include "Task_UnoComm.h"
+#include <FreeRTOS.h>
+#include "task.h"
+#include "arlo/Arlo.h"
+
+//extern Bool liftProcessFinished;
+int angle=0;
+int distance=0;
+extern Bool pick_up_process_finished;
+extern Bool drop_off_process_finished;
+extern Bool process_running;
+
+//extern Bool requestedByUno;
+
+extern Pick_Up_Status pick_up_status_t;
+extern Drop_Off_Status drop_off_status_t;
+
+uint8_t object_counter=1;
+
+typedef enum
+{
+	PICK_UP_START,PICK_UP_DO,DROP_OFF
+} states;
+
+states current_state = PICK_UP_START;
+states next_state;
+extern Bool arloNeedsToDrive;
+extern Bool arloIsDone;
+
+uint8_t already_lifted = 0;
+uint8_t already_dropped = 0;
+uint8_t objects_left = 3;
+
+uint8_t pick_up_tries = 0;
+uint8_t number_of_rotations = 0;	// Used for
+
+void task_unoComm(void *pvParameters)
+{
+	portTickType xLastWakeTime;
+	const portTickType xTimeIncrement = 1000;
+
+	xLastWakeTime = xTaskGetTickCount(); // Initialize the xLastWakeTime variable with the current time.
+
+	while (1)
+	{
+		switch (current_state)
+		{
+			///////////////////////////Start-state//////////////////////////////////////
+			
+			case PICK_UP_START:
+			printf("PICK_UP_START\r\n");
+			arlo_pick_up_object(OBJECT);
+			next_state = PICK_UP_DO;
+			break;
+			/************************************************************************/
+			/*                              PICK_UP DO, START INRE CASE             */
+			/************************************************************************/
+			case PICK_UP_DO:
+			printf("PICK_UP_DO\r\n");
+			pick_up_status_t = arlo_get_pick_up_status();
+			
+			switch (pick_up_status_t)
+			{
+				/************************************************************************/
+				/*                      --PICK_UP FAILED CASE--                         */
+				/************************************************************************/
+				case PICK_UP_FAILED:
+				printf("PICK_UP_FAILED\r\n");
+// 				if (!arloNeedsToDrive)
+// 				{
+// 					arloNeedsToDrive = true;
+// 					angle = 10;
+// 				}
+// 				if (arloIsDone)
+// 				{
+// 					arloNeedsToDrive = false;
+// 					arloIsDone=false;
+// 					arlo_done_drive(PICK_UP_DONE_DRIVE);
+// 					next_state = PICK_UP_START;
+// 				}
+				break;
+				/************************************************************************/
+				/*                      --PICK_UP FORWARD CASE--                        */
+				/************************************************************************/
+				case PICK_UP_FORWARD:
+				printf("PICK_UP_FORWARD\r\n");
+// 				if (!arloNeedsToDrive)
+// 				{
+// 					distance = 10;
+// 					arloNeedsToDrive = true;
+// 				}
+// 				if (arloIsDone)
+// 				{
+// 					arloNeedsToDrive = false;
+// 					arlo_done_drive(PICK_UP_DONE_DRIVE);
+// 					next_state = PICK_UP_START;
+// 				}
+				break;
+				/************************************************************************/
+				/*                      --PICK_UP BACKWARD CASE--                       */
+				/************************************************************************/
+				case PICK_UP_BACKWARD:
+				printf("PICK_UP_BACKWARD\r\n");
+// 				if (!arloNeedsToDrive)
+// 				{
+// 					distance = -10;
+// 					arloNeedsToDrive = true;
+// 				}
+// 				if (arloIsDone)
+// 				{
+// 					arloNeedsToDrive = false;
+// 					arlo_done_drive(PICK_UP_DONE_DRIVE);
+// 					next_state = PICK_UP_START;
+// 				}
+				break;
+				/************************************************************************/
+				/*                      --PICK_UP RUNNING CASE--                        */
+				/************************************************************************/
+				case PICK_UP_RUNNING:
+				
+				
+				
+				break;
+				/************************************************************************/
+				/*                      --PICK_UP IDLE CASE--                           */
+				/************************************************************************/
+				case PICK_UP_IDLE:
+				
+				break;
+				/************************************************************************/
+				/*                       --PICK_UP DONE CASE--                          */
+				/************************************************************************/
+				case PICK_UP_DONE:
+				printf("PICK_UP_DONE\r\n");
+				objects_left--;
+				// If Arlo can collect all items and there are no more objects to pick up or if it cannot collect them all
+				if ((arlo_get_collect_status() == 1 && objects_left == 0) || (arlo_get_collect_status() == 0))
+				{
+					printf("Skall lämna allt nu!!!!!!!!!!\r\n");
+					next_state = DROP_OFF;
+				}
+				else
+				{
+					printf("NÄSTA!!!!!!!!!!\r\n");
+					//printf("\n pick_up_process_finished=true\r\n");
+					next_state = PICK_UP_START;
+					
+				}
+				pick_up_process_finished=true;
+				vTaskSuspend(NULL);
+				break;
+				/************************************************************************/
+				/*                             inre case slut                           */
+				/************************************************************************/
+			}
+			break;
+			/************************************************************************/
+			/*                              DROP_OFF CASE                           */
+			/************************************************************************/
+			///////////////////////////Drop_off-state////////////////////////////////////
+			case DROP_OFF:
+			printf("DROP OFF CASE\r\n");
+			if (already_dropped == 0)
+			{
+				arlo_drop_object(OBJECT);
+				already_dropped = 1;
+				next_state = DROP_OFF;
+			}
+			else
+			{
+				drop_off_status_t = arlo_get_drop_off_status();
+				if (drop_off_status_t == DROP_OFF_DONE)
+				{
+					printf("Drop off process finished\r\n");
+					drop_off_process_finished = true;
+					pick_up_process_finished=true;
+					drop_off_status_t = 0;
+					already_dropped = 0;
+					next_state = PICK_UP_START;
+					vTaskSuspend(NULL);
+				}
+				else
+				{
+					next_state = DROP_OFF;
+				}
+			}
+			break;
+			/************************************************************************/
+			/*                             SLUT PÅ CASE                             */
+			/************************************************************************/
+		}
+		current_state = next_state;
+		vTaskDelayUntil(&xLastWakeTime, xTimeIncrement);	// Wait for the next cycle after have finished everything
+	}
+}
--- RTOS V.6/TestSchema1/src/Task_GetCordinates.c
+++ RTOS V.6/TestSchema1/src/Task_GetCordinates.c
@@ -14,30 +14,32 @@
 #include "Task_Move.h"
 
 extern Bool newData;
-uint8_t get_count = 0;
 
 void task_getCordinates(void *pvParameters)
 {
 	portTickType xLastWakeTime;
-	const portTickType xTimeIncrement = 300;
+	const portTickType xTimeIncrement = 200;
 
 	xLastWakeTime = xTaskGetTickCount();//Initialise the xLastWakeTime variable with the current time.
 	while (1) 
 	{	
-		
+		if (get_count<4)
+		{
 			int16_t coord_arr[2]={0};
 			arlo_get_position(coord_arr);
 			coord.presentX=coord_arr[0];
 			coord.presentY=coord_arr[1];
-			
-		printf("\n x : %d\r\n", coord.presentX);
-		printf("y : %d\r\n", coord.presentY);
+			//printf("\n x : %d\r\n", coord.presentX);
+			//printf("y : %d\r\n", coord.presentY);
+			get_count++;
+		}
 		
-			
+		else if (get_count>=5)
+		{
 			newData=false;
+			get_count=0;
 			vTaskSuspend(NULL);
-	
-		
+		}
 		vTaskDelayUntil(&xLastWakeTime, xTimeIncrement); // Wait for the next cycle.
 	}
 }
